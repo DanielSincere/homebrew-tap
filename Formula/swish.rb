2164d9ea2d86bf5fd22c242de78bc48acb0bd63d
Full Queue Developer <103295874+FullQueueDeveloper@users.noreply.github.com>
2023-01-30 10:33:03 -0500

    Update swish.rb

--- Formula/swish.rb
+++ Formula/swish.rb
@@ -1,8 +1,8 @@
 class Swish < Formula
   desc "Swift script runner"
   homepage "https://github.com/FullQueueDeveloper/Swish"
-  url "https://github.com/FullQueueDeveloper/Swish/archive/refs/tags/1.0.2.tar.gz"
-  sha256 "9d8bc0f6f42b6648eec6c52cb3518d4da2e13e198b4baac189c364f1a24fc872"
+  url "https://github.com/FullQueueDeveloper/Swish/archive/refs/tags/1.0.3.tar.gz"
+  sha256 "2a8c80fe20b6d39929d2075aaf4cf8d329f831be865f5eb3e7c7ba7004bec753"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
