cbe114e55c33d77e41fe0d1f09d764033f029c01
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-04-02 15:23:25 -0400

    SPX 1.1.1

--- Formula/spx.rb
+++ Formula/spx.rb
@@ -1,9 +1,10 @@
 class SPX < Formula
   desc "NPX for Swift"
   homepage "https://github.com/FullQueueDeveloper/SPX"
-  url "https://github.com/FullQueueDeveloper/SPX/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "83433261d3cf11c2215f84647c48c2ce3bd46d6828b6984b641c230cb1655661"
+  url "https://github.com/FullQueueDeveloper/SPX/archive/refs/tags/1.1.1.tar.gz"
+  sha256 "47fb02ba413e80a228536107af71e23dcc76d2e3124f7a206f853c2053d7efbe"
   # curl -sL $url | sha256sum
+  name "spx"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
@@ -11,11 +12,11 @@ class SPX < Formula
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
-    bin.install ".build/release/#{name}"
+    bin.install ".build/release/spx"
   end
 
   test do
     # Test by showing the version
-    system "#{bin}/SPX", "-v"
+    system "#{bin}/spx", "-v"
   end
 end
