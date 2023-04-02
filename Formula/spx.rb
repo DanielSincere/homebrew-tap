e00716db43bbbaa2c3f975cafd09c7e1776bea42
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-04-02 15:40:28 -0400

    weird merge

--- Formula/spx.rb
+++ Formula/spx.rb
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-class SPX < Formula
-  desc "NPX for Swift"
-  homepage "https://github.com/FullQueueDeveloper/SPX"
-  url "https://github.com/FullQueueDeveloper/SPX/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "83433261d3cf11c2215f84647c48c2ce3bd46d6828b6984b641c230cb1655661"
-  # curl -sL $url | sha256sum
-=======
 class Spx < Formula
   desc "SPX: NPX for Swift"
   homepage "https://github.com/FullQueueDeveloper/SPX"
@@ -13,7 +5,6 @@ class Spx < Formula
   sha256 "47fb02ba413e80a228536107af71e23dcc76d2e3124f7a206f853c2053d7efbe"
   # curl -sL $url | sha256sum
   name "spx"
->>>>>>> f71be28 (fix)
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
@@ -21,19 +12,11 @@ class Spx < Formula
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
-<<<<<<< HEAD
-    bin.install ".build/release/#{name}"
-=======
     bin.install ".build/release/spx"
->>>>>>> f71be28 (fix)
   end
 
   test do
     # Test by showing the version
-<<<<<<< HEAD
-    system "#{bin}/SPX", "-v"
-=======
     system "#{bin}/spx", "-v"
->>>>>>> f71be28 (fix)
   end
 end
